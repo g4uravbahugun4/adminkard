024e4a3dc5ee1a55a0fd8b32634cbec71de4cf14
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-04-22 20:56:57 +0530

    commit9

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV !== "production"
         ? "http://localhost:3000"
-        : "https://puce-wild-xerus.cyclic.app"
+        : "https://adminkard-bgs9.vercel.app/"
 
 module.exports = baseUrl;
